86c78a0ebe81fa3e6324ec614fbf1c364bc0c93b
Ridho <ridho.hikaru@gmail.com>
2020-07-10 18:30:46 +0700

    Add enemy on map.

--- game/source/exploration/sprite_interactive.gd
+++ game/source/exploration/sprite_interactive.gd
@@ -7,3 +7,14 @@ export var vanish_on_interaction: = false
 export var AUTO_START_INTERACTION: = false
 export var sight_distance = 50
 
+onready var area = $area
+
+func _ready():
+	area.connect("body_entered", self, "_on_body_entered")
+
+func _on_body_entered(body: PhysicsBody) -> void:
+	if body.is_in_group("player"):
+		if AUTO_START_INTERACTION:
+			print("AUTO")
+		else:
+			print("MANUAL")
